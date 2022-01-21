f2b33bab4cb05e12bb9455a6004e5ba2197a7fd1
jhorn00 <dawshorn@gmail.com>
2022-01-21 17:52:52 -0500

    Added: Info files, Updated: Naming Scheme

--- Benchmark_Graphs.jl
+++ Benchmark_Graphs.jl
@@ -22,21 +22,21 @@ testdatadir = joinpath(dirname(@__FILE__), "..", "test", "testdata")
 dg1fn = joinpath(testdatadir, "graph-50-500.jgz")
 
 LG_GRAPHS = Dict{String,LG.DiGraph}(
-    "complete100"   => LG.complete_digraph(100),
+    "complete100" => LG.complete_digraph(100),
     # "5000-50000"    => LG.loadgraph(dg1fn)["graph-5000-50000"],
-    "path500"       => LG.path_digraph(500)
+    "path500" => LG.path_digraph(500)
 )
 
-GRAPHS = Dict(k => from_lightgraph(g) for (k,g) in LG_GRAPHS)
+GRAPHS = Dict(k => from_lightgraph(g) for (k, g) in LG_GRAPHS)
 
 LG_SYMGRAPHS = Dict{String,LG.Graph}(
-    "complete100"   => LG.complete_graph(100),
-    "tutte"         => LG.smallgraph(:tutte),
-    "path500"       => LG.path_graph(500),
+    "complete100" => LG.complete_graph(100),
+    "tutte" => LG.smallgraph(:tutte),
+    "path500" => LG.path_graph(500),
     # "5000-49947"    => LG.SimpleGraph(DIGRAPHS["5000-50000"])
 )
 
-SYMGRAPHS = Dict(k => from_lightgraph(g) for (k,g) in LG_SYMGRAPHS)
+SYMGRAPHS = Dict(k => from_lightgraph(g) for (k, g) in LG_SYMGRAPHS)
 
 # Helpers
 #########
@@ -45,40 +45,40 @@ SYMGRAPHS = Dict(k => from_lightgraph(g) for (k,g) in LG_SYMGRAPHS)
 # https://github.com/JuliaGraphs/LightGraphs.jl/blob/master/benchmark/core.jl
 
 function bench_iter_edges(g)
-  count = 0
-  for e in edges(g)
-    s, t = src(g,e), tgt(g,e)
-    count += 1
-  end
-  count
+    count = 0
+    for e in edges(g)
+        s, t = src(g, e), tgt(g, e)
+        count += 1
+    end
+    count
 end
 function bench_iter_edges_vectorized(g)
-  count = 0
-  for (s,t) in zip(src(g), tgt(g))
-    count += 1
-  end
-  count
+    count = 0
+    for (s, t) in zip(src(g), tgt(g))
+        count += 1
+    end
+    count
 end
 
 function bench_has_edge(g)
-  Random.seed!(1)
-  n = nv(g)
-  srcs, tgts = rand(1:n, n÷4), rand(1:n, n÷4)
-  count = 0
-  for (s, t) in zip(srcs, tgts)
-    if has_edge(g, s, t)
-      count += 1
+    Random.seed!(1)
+    n = nv(g)
+    srcs, tgts = rand(1:n, n ÷ 4), rand(1:n, n ÷ 4)
+    count = 0
+    for (s, t) in zip(srcs, tgts)
+        if has_edge(g, s, t)
+            count += 1
+        end
     end
-  end
-  count
+    count
 end
 
 function bench_iter_neighbors(g)
-  count = 0
-  for v in vertices(g)
-    count += length(neighbors(g,v))
-  end
-  count
+    count = 0
+    for v in vertices(g)
+        count += length(neighbors(g, v))
+    end
+    count
 end
 
 @inline Graphs.nv(g::LG.AbstractGraph) = LG.nv(g)
@@ -90,9 +90,9 @@ end
 @inline Graphs.neighbors(g::LG.AbstractGraph, args...) = LG.neighbors(g, args...)
 
 function Graphs.connected_component_projection(g::LG.AbstractGraph)
-  label = Vector{Int}(undef, LG.nv(g))
-  LG.connected_components!(label, g)
-  normalize_labeling(label)
+    label = Vector{Int}(undef, LG.nv(g))
+    LG.connected_components!(label, g)
+    normalize_labeling(label)
 end
 
 abstract type FindTrianglesAlgorithm end
@@ -101,18 +101,18 @@ struct TriangleQuery <: FindTrianglesAlgorithm end
 
 """ Number of triangles in a graph.
 """
-function ntriangles(g::T, ::TriangleBacktrackingSearch) where T
-  triangle = T(3)
-  add_edges!(triangle, [1,2,1], [2,3,3])
-  length(homomorphisms(triangle, g, alg=BacktrackingSearch()))
+function ntriangles(g::T, ::TriangleBacktrackingSearch) where {T}
+    triangle = T(3)
+    add_edges!(triangle, [1, 2, 1], [2, 3, 3])
+    length(homomorphisms(triangle, g, alg = BacktrackingSearch()))
 end
 function ntriangles(g, ::TriangleQuery)
-  length(query(g, ntriangles_query))
+    length(query(g, ntriangles_query))
 end
 const ntriangles_query = @relation (;) begin
-  E(src=v1, tgt=v2)
-  E(src=v2, tgt=v3)
-  E(src=v1, tgt=v3)
+    E(src = v1, tgt = v2)
+    E(src = v2, tgt = v3)
+    E(src = v1, tgt = v3)
 end
 
 # Graphs
@@ -124,13 +124,13 @@ clvecbench = bench["Catlab-vectorized"] = BenchmarkGroup()
 lgbench = bench["LightGraphs"] = BenchmarkGroup()
 
 n = 10000
-clbench["make-path"] = @benchmarkable path_graph(Graph,n)
+clbench["make-path"] = @benchmarkable path_graph(Graph, n)
 lgbench["make-path"] = @benchmarkable begin
-  g = LG.DiGraph()
-  LG.add_vertices!(g, n)
-  for v in 1:(n-1)
-    LG.add_edge!(g, v, v+1)
-  end
+    g = LG.DiGraph()
+    LG.add_vertices!(g, n)
+    for v in 1:(n-1)
+        LG.add_edge!(g, v, v + 1)
+    end
 end
 
 g = path_graph(Graph, n)
@@ -154,21 +154,21 @@ g₀ = path_graph(Graph, n₀)
 g = ob(coproduct(fill(g₀, 5)))
 lg = LG.DiGraph(g)
 clbench["path-graph"] =
-  @benchmarkable connected_component_projection_bfs($g)
+    @benchmarkable connected_component_projection_bfs($g)
 lgbench["path-graph"] =
-  @benchmarkable connected_component_projection($lg)
+    @benchmarkable connected_component_projection($lg)
 
 g₀ = star_graph(Graph, n₀)
 g = ob(coproduct(fill(g₀, 5)))
 lg = LG.DiGraph(g)
 clbench["star-graph"] =
-  @benchmarkable connected_component_projection_bfs($g)
+    @benchmarkable connected_component_projection_bfs($g)
 lgbench["star-graph"] =
-  @benchmarkable connected_component_projection($lg)
+    @benchmarkable connected_component_projection($lg)
 
 for gn in keys(GRAPHS)
-  clbench[gn] = @benchmarkable connected_component_projection_bfs($(GRAPHS[gn]))
-  lgbench[gn] = @benchmarkable connected_component_projection($(LG_GRAPHS[gn]))
+    clbench[gn] = @benchmarkable connected_component_projection_bfs($(GRAPHS[gn]))
+    lgbench[gn] = @benchmarkable connected_component_projection($(LG_GRAPHS[gn]))
 end
 
 bench = SUITE["GraphTriangles"] = BenchmarkGroup()
@@ -179,9 +179,9 @@ n = 100
 g = wheel_graph(Graph, n)
 lg = LG.DiGraph(g)
 clbench["wheel-graph-triangles-hom"] =
-  @benchmarkable ntriangles($g, TriangleBacktrackingSearch())
+    @benchmarkable ntriangles($g, TriangleBacktrackingSearch())
 clbench["wheel-graph-triangles"] =
-  @benchmarkable ntriangles($g, TriangleQuery())
+    @benchmarkable ntriangles($g, TriangleQuery())
 
 # Symmetric graphs
 ##################
@@ -194,11 +194,11 @@ lgbench = bench["LightGraphs"] = BenchmarkGroup()
 n = 10000
 clbench["make-path"] = @benchmarkable path_graph(SymmetricGraph, n)
 lgbench["make-path"] = @benchmarkable begin
-  g = LG.Graph()
-  LG.add_vertices!(g, n)
-  for v in 1:(n-1)
-    LG.add_edge!(g, v, v+1)
-  end
+    g = LG.Graph()
+    LG.add_vertices!(g, n)
+    for v in 1:(n-1)
+        LG.add_edge!(g, v, v + 1)
+    end
 end
 
 g = path_graph(SymmetricGraph, n)
@@ -221,21 +221,21 @@ g₀ = path_graph(SymmetricGraph, n₀)
 g = ob(coproduct(fill(g₀, 5)))
 lg = LG.Graph(g)
 clbench["path-graph-components"] =
-  @benchmarkable connected_component_projection($g)
+    @benchmarkable connected_component_projection($g)
 lgbench["path-graph-components"] =
-  @benchmarkable connected_component_projection($lg)
+    @benchmarkable connected_component_projection($lg)
 
 g₀ = star_graph(SymmetricGraph, n₀)
 g = ob(coproduct(fill(g₀, 5)))
 lg = LG.Graph(g)
 clbench["star-graph-components"] =
-  @benchmarkable connected_component_projection($g)
+    @benchmarkable connected_component_projection($g)
 lgbench["star-graph-components"] =
-  @benchmarkable connected_component_projection($lg)
+    @benchmarkable connected_component_projection($lg)
 
 for gn in keys(SYMGRAPHS)
-  clbench[gn] = @benchmarkable connected_component_projection_bfs($(SYMGRAPHS[gn]))
-  lgbench[gn] = @benchmarkable connected_component_projection($(LG_SYMGRAPHS[gn]))
+    clbench[gn] = @benchmarkable connected_component_projection_bfs($(SYMGRAPHS[gn]))
+    lgbench[gn] = @benchmarkable connected_component_projection($(LG_SYMGRAPHS[gn]))
 end
 
 bench = SUITE["SymmetricGraphTriangles"] = BenchmarkGroup()
@@ -246,10 +246,10 @@ n = 100
 g = wheel_graph(SymmetricGraph, n)
 lg = LG.Graph(g)
 clbench["wheel-graph-triangles-hom"] =
-  @benchmarkable ntriangles($g, TriangleBacktrackingSearch())
+    @benchmarkable ntriangles($g, TriangleBacktrackingSearch())
 # clbench["wheel-graph-triangles-query"] =
 clbench["wheel-graph-triangles"] =
-  @benchmarkable ntriangles($g, TriangleQuery())
+    @benchmarkable ntriangles($g, TriangleQuery())
 lgbench["wheel-graph-triangles"] = @benchmarkable sum(LG.triangles($lg))
 
 # Weighted graphs
@@ -261,38 +261,38 @@ clvecbench = bench["Catlab-vectorized"] = BenchmarkGroup()
 lgbench = bench["MetaGraphs"] = BenchmarkGroup()
 
 n = 10000
-g = path_graph(WeightedGraph{Float64}, n; E=(weight=range(0,1,length=n-1),))
+g = path_graph(WeightedGraph{Float64}, n; E = (weight = range(0, 1, length = n - 1),))
 mg = MG.MetaDiGraph(g)
 
 clvecbench["sum-weights"] = @benchmarkable sum(weight($g))
 clbench["sum-weights"] = @benchmarkable begin
-  total = 0.0
-  for e in edges($g)
-    total += weight($g, e)
-  end
-  total
+    total = 0.0
+    for e in edges($g)
+        total += weight($g, e)
+    end
+    total
 end
 lgbench["sum-weights"] = @benchmarkable begin
-  total = 0.0
-  for e in MG.edges($mg)
-    total += MG.get_prop($mg, e, :weight)
-  end
-  total
+    total = 0.0
+    for e in MG.edges($mg)
+        total += MG.get_prop($mg, e, :weight)
+    end
+    total
 end
 
 clvecbench["increment-weights"] = @benchmarkable begin
-  $g[:weight] .= $g[:weight] .+ 1.0
+    $g[:weight] .= $g[:weight] .+ 1.0
 end
 
 clbench["increment-weights"] = @benchmarkable begin
-  for e in edges($g)
-    $g[e,:weight] += 1.0
-  end
+    for e in edges($g)
+        $g[e, :weight] += 1.0
+    end
 end
 lgbench["increment-weights"] = @benchmarkable begin
-  for e in MG.edges($mg)
-    MG.set_prop!($mg, e, :weight, MG.get_prop($mg, e, :weight) + 1.0)
-  end
+    for e in MG.edges($mg)
+        MG.set_prop!($mg, e, :weight, MG.get_prop($mg, e, :weight) + 1.0)
+    end
 end
 
 # Labeled graphs
@@ -303,63 +303,65 @@ clbench = bench["Catlab"] = BenchmarkGroup()
 lgbench = bench["LightGraphs"] = BenchmarkGroup()
 
 @present TheoryLabeledGraph <: TheoryGraph begin
-  Label::AttrType
-  label::Attr(V,Label)
+    Label::AttrType
+    label::Attr(V, Label)
 end
-@acset_type LabeledGraph(TheoryLabeledGraph, index=[:src,:tgt]) <: AbstractGraph
-@acset_type IndexedLabeledGraph(TheoryLabeledGraph, index=[:src,:tgt],
-                                unique_index=[:label]) <: AbstractGraph
+@acset_type LabeledGraph(TheoryLabeledGraph, index = [:src, :tgt]) <: AbstractGraph
+@acset_type IndexedLabeledGraph(TheoryLabeledGraph, index = [:src, :tgt],
+    unique_index = [:label]) <: AbstractGraph
 
-function discrete_labeled_graph(n::Int; indexed::Bool=false)
-  g = (indexed ? IndexedLabeledGraph{String} : LabeledGraph{String})()
-  add_vertices!(g, n, label=("v$i" for i in 1:n))
-  g
+function discrete_labeled_graph(n::Int; indexed::Bool = false)
+    g = (indexed ? IndexedLabeledGraph{String} : LabeledGraph{String})()
+    add_vertices!(g, n, label = ("v$i" for i in 1:n))
+    g
 end
 
-function discrete_labeled_metagraph(n::Int; indexed::Bool=false)
-  mg = MG.MetaDiGraph()
-  for i in 1:n
-    MG.add_vertex!(mg, :label, "v$i")
-  end
-  if indexed; MG.set_indexing_prop!(mg, :label) end
-  mg
+function discrete_labeled_metagraph(n::Int; indexed::Bool = false)
+    mg = MG.MetaDiGraph()
+    for i in 1:n
+        MG.add_vertex!(mg, :label, "v$i")
+    end
+    if indexed
+        MG.set_indexing_prop!(mg, :label)
+    end
+    mg
 end
 
 n = 5000
 clbench["make-discrete"] = @benchmarkable discrete_labeled_graph($n)
 lgbench["make-discrete"] = @benchmarkable discrete_labeled_metagraph($n)
 clbench["make-discrete-indexed"] =
-  @benchmarkable discrete_labeled_graph($n, indexed=true)
+    @benchmarkable discrete_labeled_graph($n, indexed = true)
 lgbench["make-discrete-indexed"] =
-  @benchmarkable discrete_labeled_metagraph($n, indexed=true)
+    @benchmarkable discrete_labeled_metagraph($n, indexed = true)
 
 n = 10000
 g = discrete_labeled_graph(n)
 mg = discrete_labeled_metagraph(n)
 clbench["iter-labels"] = @benchmarkable begin
-  for v in vertices($g)
-    label = $g[v,:label]
-  end
+    for v in vertices($g)
+        label = $g[v, :label]
+    end
 end
 lgbench["iter-labels"] = @benchmarkable begin
-  for v in MG.vertices($mg)
-    label = MG.get_prop($mg, v, :label)
-  end
+    for v in MG.vertices($mg)
+        label = MG.get_prop($mg, v, :label)
+    end
 end
 
-g = discrete_labeled_graph(n, indexed=true)
-mg = discrete_labeled_metagraph(n, indexed=true)
+g = discrete_labeled_graph(n, indexed = true)
+mg = discrete_labeled_metagraph(n, indexed = true)
 Random.seed!(1)
 σ = randperm(n)
 clbench["indexed-lookup"] = @benchmarkable begin
-  for i in $σ
-    @assert incident($g, "v$i", :label) == i
-  end
+    for i in $σ
+        @assert incident($g, "v$i", :label) == i
+    end
 end
 lgbench["indexed-lookup"] = @benchmarkable begin
-  for i in $σ
-    @assert $mg["v$i", :label] == i
-  end
+    for i in $σ
+        @assert $mg["v$i", :label] == i
+    end
 end
 
 # Random Graphs
@@ -372,26 +374,26 @@ lgbench = bench["LightGraphs"] = BenchmarkGroup()
 sizes = [10000]
 ps = [0.001]
 for size in sizes, p in ps
-  clbench["erdos_renyi-$size-$p"] =
-    @benchmarkable erdos_renyi($Graph, $size, $(p/2))
-  lgbench["erdos_renyi-$size-$p"] =
-    @benchmarkable LightGraphs.erdos_renyi($size, $p)
+    clbench["erdos_renyi-$size-$p"] =
+        @benchmarkable erdos_renyi($Graph, $size, $(p / 2))
+    lgbench["erdos_renyi-$size-$p"] =
+        @benchmarkable LightGraphs.erdos_renyi($size, $p)
 end
 
 ks = [10]
 
 for size in sizes, k in ks
-  clbench["expected_degree_graph-$size-$k"] =
-    @benchmarkable expected_degree_graph($Graph, $([min(k,size-1) for _ in 1:size]))
-  lgbench["expected_degree_graph-$size-$k"] =
-    @benchmarkable LightGraphs.expected_degree_graph($([min(k,size-1) for _ in 1:size]))
+    clbench["expected_degree_graph-$size-$k"] =
+        @benchmarkable expected_degree_graph($Graph, $([min(k, size - 1) for _ in 1:size]))
+    lgbench["expected_degree_graph-$size-$k"] =
+        @benchmarkable LightGraphs.expected_degree_graph($([min(k, size - 1) for _ in 1:size]))
 end
 
 for size in sizes, k in ks
-  clbench["watts_strogatz-$size-$k"] =
-    @benchmarkable watts_strogatz($Graph, $size, $(min(k,size-1)), 0.5)
-  lgbench["watts_strogatz-$size-$k"] =
-    @benchmarkable LightGraphs.watts_strogatz($size, $(min(k,size-1)), 0.5)
+    clbench["watts_strogatz-$size-$k"] =
+        @benchmarkable watts_strogatz($Graph, $size, $(min(k, size - 1)), 0.5)
+    lgbench["watts_strogatz-$size-$k"] =
+        @benchmarkable LightGraphs.watts_strogatz($size, $(min(k, size - 1)), 0.5)
 end
 
 # Searching
@@ -402,10 +404,10 @@ clbench = bench["Catlab"] = BenchmarkGroup()
 lgbench = bench["LightGraphs"] = BenchmarkGroup()
 
 for size in sizes, p in ps
-  local g = erdos_renyi(Graph, size, p)
-  local lg = LightGraphs.SimpleDiGraph(g)
-  clbench["bfs_erdos_renyi-$size-$p"] = @benchmarkable bfs_parents($g,1)
-  lgbench["bfs_erdos_renyi-$size-$p"] = @benchmarkable LightGraphs.bfs_parents($lg,1)
-  clbench["dfs_erdos_renyi-$size-$p"] = @benchmarkable dfs_parents($g,1)
-  lgbench["dfs_erdos_renyi-$size-$p"] = @benchmarkable LightGraphs.dfs_parents($lg,1)
+    local g = erdos_renyi(Graph, size, p)
+    local lg = LightGraphs.SimpleDiGraph(g)
+    clbench["bfs_erdos_renyi-$size-$p"] = @benchmarkable bfs_parents($g, 1)
+    lgbench["bfs_erdos_renyi-$size-$p"] = @benchmarkable LightGraphs.bfs_parents($lg, 1)
+    clbench["dfs_erdos_renyi-$size-$p"] = @benchmarkable dfs_parents($g, 1)
+    lgbench["dfs_erdos_renyi-$size-$p"] = @benchmarkable LightGraphs.dfs_parents($lg, 1)
 end
---
+++ SimplicialSets.jl
@@ -0,0 +1,173 @@
+module TestSimplicialSets
+using Test
+
+using SparseArrays, StaticArrays
+
+using Catlab.CategoricalAlgebra.CSets
+using CombinatorialSpaces.SimplicialSets
+
+""" Check that the semi-simplicial identities hold in dimension `n`.
+"""
+function is_semi_simplicial(s::HasDeltaSet, n::Int)
+    all(∂(n - 1, i, s, ∂(n, j, s)) == ∂(n - 1, j - 1, s, ∂(n, i, s))
+        for i in 0:n for j in (i+1):n)
+end
+
+const Point2D = SVector{2,Float64}
+const Point3D = SVector{3,Float64}
+
+# 1D simplicial sets
+####################
+
+s = DeltaSet1D()
+add_vertex!(s)
+add_vertices!(s, 3)
+add_sorted_edge!(s, 2, 1)
+add_sorted_edges!(s, [2, 4], [3, 3])
+@test (nv(s), ne(s)) == (4, 3)
+@test (vertices(s), edges(s)) == (1:4, 1:3)
+@test src(s) == [1, 2, 3]
+@test tgt(s) == [2, 3, 4]
+@test has_edge(s, 1, 2)
+@test !has_edge(s, 2, 1)
+@test ∂(1, 0, s) == [2, 3, 4]
+@test ∂(1, 1, s) == [1, 2, 3]
+@test ∂(0, s, E(1))::V == V(2)
+@test ∂(1, s, E([1, 3]))::V == V([1, 3])
+@test coface(0, s, V(4))::E == E([3])
+
+# 1D oriented simplicial sets
+#----------------------------
+
+s = OrientedDeltaSet1D{Bool}()
+add_vertices!(s, 4)
+add_edges!(s, [1, 2, 3], [2, 3, 4], edge_orientation = [true, false, true])
+@test ∂(1, s, 1) == [-1, 1, 0, 0]
+@test ∂(1, s, 2) == [0, 1, -1, 0]
+
+# Boundary operator, dense vectors.
+vvec = ∂(1, s, 1, Vector{Int})
+@test !issparse(vvec)
+@test vvec == [-1, 1, 0, 0]
+vvec = ∂(1, s, [1, -1, 1])
+@test !issparse(vvec)
+@test vvec == [-1, 0, 0, 1]
+@test ∂(s, EChain([1, -1, 1]))::VChain == VChain(vvec)
+
+# Boundary operator, sparse vectors.
+vvec = ∂(1, s, 1, SparseVector{Int})
+@test issparse(vvec)
+@test vvec == [-1, 1, 0, 0]
+vvec = ∂(1, s, sparsevec([1, -1, 1]))
+@test issparse(vvec)
+@test vvec == [-1, 0, 0, 1]
+@test ∂(s, EChain(sparsevec([1, -1, 1]))) == VChain(vvec)
+B = ∂(1, s)
+@test issparse(B)
+@test B * [1, -1, 1] == [-1, 0, 0, 1]
+
+# Exterior derivative.
+@test d(s, VForm([1, 1, 1, 4]))::EForm == EForm([0, 0, 3])
+@test d(s, VForm([4, 1, 0, 0])) == EForm([-3, 1, 0])
+@test d(0, s) == B'
+
+# Consistent orientation.
+s = OrientedDeltaSet1D{Bool}()
+add_vertices!(s, 10)
+add_edges!(s, 1:4, 2:5)
+add_edges!(s, 6:9, 7:10)
+@test orient_component!(s, 1, true)
+@test orientation(s, E(1:4)) == trues(4)
+@test orient_component!(s, 8, true)
+@test orientation(s, E(1:8)) == trues(8)
+
+s[:edge_orientation] = rand(Bool, 8)
+@test orient!(s)
+@test orientation(s, E(1:8)) == trues(8)
+
+# 1D embedded simplicial sets
+#----------------------------
+
+s = EmbeddedDeltaSet1D{Bool,Point2D}()
+add_vertices!(s, 2, point = [Point2D(-1, 0), Point2D(+1, 0)])
+add_edge!(s, 1, 2, edge_orientation = true)
+@test volume(1, s, 1) ≈ 2
+@test volume(s, E(1)) ≈ 2
+
+# 2D simplicial sets
+####################
+
+s = DeltaSet2D()
+add_vertices!(s, 3)
+glue_triangle!(s, 1, 2, 3)
+@test is_semi_simplicial(s, 2)
+@test ntriangles(s) == 1
+@test map(i -> ∂(2, i, s, 1), (0, 1, 2)) == (2, 3, 1)
+@test triangle_vertices(s, 1) == [1, 2, 3]
+
+s′ = DeltaSet2D()
+add_vertices!(s′, 3)
+glue_sorted_triangle!(s′, 2, 3, 1)
+@test s′ == s
+
+# Triangulated commutative square.
+s = DeltaSet2D()
+add_vertices!(s, 4)
+glue_triangle!(s, 1, 2, 3)
+glue_triangle!(s, 1, 4, 3)
+@test ntriangles(s) == 2
+@test triangles(s) == 1:2
+@test ne(s) == 5
+@test sort(map(Pair, src(s), tgt(s))) == [1 => 2, 1 => 3, 1 => 4, 2 => 3, 4 => 3]
+
+# 2D oriented simplicial sets
+#----------------------------
+
+# Triangle with matching edge orientations.
+s = OrientedDeltaSet2D{Bool}()
+add_vertices!(s, 3)
+add_sorted_edges!(s, [1, 2, 3], [2, 3, 1], edge_orientation = [true, true, false])
+glue_triangle!(s, 1, 2, 3)
+@test orient_component!(s, 1, true)
+@test orientation(s, Tri(1)) == true
+@test ∂(2, s, 1) == [1, 1, 1]
+@test d(1, s, [45, 3, 34]) == [82]
+
+# Triangulated square with consistent orientation.
+s = OrientedDeltaSet2D{Bool}()
+add_vertices!(s, 4)
+glue_triangle!(s, 1, 2, 3)
+glue_triangle!(s, 1, 3, 4)
+s[:edge_orientation] = true
+@test orient!(s)
+@test orientation(s, Tri(1:2)) == trues(2)
+@test ∂(2, s, 1) == [1, 1, -1, 0, 0]
+@test ∂(s, TriChain([1, 1]))::EChain == EChain([1, 1, 0, 1, -1])
+@test d(s, EForm([45, 3, 34, 0, 0]))::TriForm == TriForm([14, 34]) # == [45+3-34, 34]
+@test d(s, EForm([45, 3, 34, 17, 5])) == TriForm([14, 46]) # == [45+3-34, 34+17-5]
+@test d(1, s) == ∂(2, s)'
+
+# 2D embedded simplicial sets
+#----------------------------
+
+# Standard 2-simplex in ℝ³.
+s = EmbeddedDeltaSet2D{Bool,Point3D}()
+add_vertices!(s, 3, point = [Point3D(1, 0, 0), Point3D(0, 1, 0), Point3D(0, 0, 1)])
+glue_triangle!(s, 1, 2, 3, tri_orientation = true)
+@test volume(s, Tri(1)) ≈ sqrt(3) / 2
+
+# Euclidean geometry
+####################
+
+std_simplex_volume(n::Int) = sqrt(n + 1) / factorial(n)
+
+p1, p2 = Point2D(1, 0), Point2D(0, 1)
+@test volume([p1, p2]) ≈ std_simplex_volume(1)
+
+p1, p2, p3 = Point3D(1, 0, 0), Point3D(0, 1, 0), Point3D(0, 0, 1)
+@test volume([p1, p2, p3]) ≈ std_simplex_volume(2)
+
+p1, p2, p3, p4 = SVector(1, 0, 0, 0), SVector(0, 1, 0, 0), SVector(0, 0, 1, 0), SVector(0, 0, 0, 1)
+@test volume([p1, p2, p3, p4]) ≈ std_simplex_volume(3)
+
+end
