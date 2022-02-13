5ee06d610485e6b5a8ca11f148f7103c34a57f78
jhorn00 <dawshorn@gmail.com>
2022-02-12 22:28:51 -0500

    checkpoint 2/12

--- Initial Analysis/graphHoms2.jl
+++ Initial Analysis/graphHoms2.jl
@@ -3,6 +3,7 @@ using Catlab, Catlab.Theories
 using Catlab.CategoricalAlgebra
 using Catlab.Graphs
 using Catlab.Graphics
+using Catlab.CSets
 using Catlab.Graphics.Graphviz
 using Colors
 using Plots
@@ -75,6 +76,70 @@ include("graph_bank.jl")
 # const to = TimerOutput()
 
 
+
+
+
+# function backtracking_search(f, X::StructACSet{S}, Y::StructACSet{S};
+#     monic=false, iso=false, initial=(;)) where {Ob, S<:SchemaDescType{Ob}}
+# # Fail early if no monic/isos exist on cardinality grounds.
+# if iso isa Bool
+# iso = iso ? Ob : ()
+# end
+# for c in iso
+# nparts(X,c) == nparts(Y,c) || return false
+# end
+# if monic isa Bool
+# monic = monic ? Ob : ()
+# end
+# # Injections between finite sets are bijections, so reduce to that case.
+# monic = unique([iso..., monic...])
+# for c in monic
+# nparts(X,c) <= nparts(Y,c) || return false
+# end
+
+# # Initialize state variables for search.
+# assignment = NamedTuple{Ob}(zeros(Int, nparts(X, c)) for c in Ob)
+# assignment_depth = map(copy, assignment)
+# inv_assignment = NamedTuple{Ob}(
+# (c in monic ? zeros(Int, nparts(Y, c)) : nothing) for c in Ob)
+# state = BacktrackingState(assignment, assignment_depth, inv_assignment, X, Y)
+
+# # Make any initial assignments, failing immediately if inconsistent.
+# for (c, c_assignments) in pairs(initial)
+# for (x, y) in partial_assignments(c_assignments)
+# assign_elem!(state, 0, Val{c}, x, y) || return false
+# end
+# end
+
+# # Start the main recursion for backtracking search.
+# backtracking_search(f, state, 1)
+# end
+
+# function backtracking_search(f, state::BacktrackingState, depth::Int)
+# # Choose the next unassigned element.
+# mrv, mrv_elem = find_mrv_elem(state, depth)
+# if isnothing(mrv_elem)
+# # No unassigned elements remain, so we have a complete assignment.
+# return f(ACSetTransformation(state.assignment, state.dom, state.codom))
+# elseif mrv == 0
+# # An element has no allowable assignment, so we must backtrack.
+# return false
+# end
+# c, x = mrv_elem
+
+# # Attempt all assignments of the chosen element.
+# Y = state.codom
+# for y in parts(Y, c)
+# assign_elem!(state, depth, Val{c}, x, y) &&
+# backtracking_search(f, state, depth + 1) &&
+# return true
+# unassign_elem!(state, depth, Val{c}, x)
+# end
+# return false
+# end
+
+
+
 homomorphism(X::ACSet, Y::ACSet; alg = BacktrackingSearch(), kw...) =
     homomorphism(X, Y, alg; kw...)
 
@@ -89,9 +154,4 @@ end
 
 
 
-
-
-h = length(homomorphisms(line_four, directional_2x2))
-h1 = homomorphism(line_four, directional_2x2, 5)
-
-print(h)
+h1 = homomorphism(line_four, directional_2x2)
